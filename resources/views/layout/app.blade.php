6ef0b5471d2ff624ffdc133a772f3e5942ed1855
echayotte <echayotte@gmail.com>
2019-02-14 16:22:11 +0100

    refacto links

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,9 +9,9 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
         <link rel="stylesheet" href="/css/app.css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
 
     <body>
