a383db115cb5ce754f18d1079c9332db78751e09
echayotte <echayotte@gmail.com>
2019-02-14 17:10:34 +0100

    add sound writing in seed migration

--- database/seeds/MediaTableSeeder.php
+++ database/seeds/MediaTableSeeder.php
@@ -15,7 +15,8 @@ class MediaTableSeeder extends Seeder
     {
 
         $medias = [
-            ['media_id' => 1, 'media_type' => 'son', 'media_filename' => 'plouf.mp3', 'media_path' => '/storage/medias/plouf.mp3', 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')]
+            ['media_id' => 1, 'media_type' => 'son', 'media_filename' => 'plouf.mp3', 'media_path' => '/storage/medias/plouf.mp3', 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')],
+            ['media_id' => 2, 'media_type' => 'son', 'media_filename' => 'writing.mp3', 'media_path' => '/storage/medias/writing.mp3', 'created_at' => date('Y-m-d H:i:s'),  'updated_at' => date('Y-m-d H:i:s')]
         ];
 
         Media::insert($medias);
